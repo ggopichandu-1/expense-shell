6cdc136834683b9f6f965c980c975a4ffb8f076b
ggopichandu <gummagopichand@gmail.com>
2024-03-03 17:11:50 +0530

    changing the order of backend.service file copy

--- backend.sh
+++ backend.sh
@@ -7,8 +7,11 @@ useradd expense
 
 cp backend.service /etc/systemd/system/backend.service
 
+rm -rf /app
+
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
+
 cd /app
 unzip /tmp/backend.zip
 
